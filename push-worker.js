a48f401d5318445cb434328f803c0975594f0eda
Jade Westover <jade424433@gmail.com>
2018-09-06 16:15:38 -0700

    added UA thing

---
+++ push-worker.js
@@ -0,0 +1,10 @@
+// 86acbd31cd7c09cf30acb66d2fbedc91daa48b86:1536274630.24
+importScripts('https://web-sdk.urbanairship.com/notify/v1/ua-sdk.min.js')
+uaSetup.worker(self, {
+  defaultIcon: 'https://www.google.com/url?sa\u003Di\u0026rct\u003Dj\u0026q\u003D\u0026esrc\u003Ds\u0026source\u003Dimages\u0026cd\u003D\u0026cad\u003Drja\u0026uact\u003D8\u0026ved\u003D2ahUKEwiD\u002D6rwvKfdAhVyp1kKHXH6BF4QjRx6BAgBEAU\u0026url\u003Dhttps%3A%2F%2Fwww.urbanairship.com%2F\u0026psig\u003DAOvVaw0NyTVoEVtf5xC2RziQq_GM\u0026ust\u003D1536361002142909',
+  defaultTitle: 'TestWebNotify',
+  defaultActionURL: 'https://jahdeh.github.io/TestWebNotify/',
+  appKey: 'k_N_4bKERXeMnGSsS5nMaA',
+  token: 'MTprX05fNGJLRVJYZU1uR1NzUzVuTWFBOkJYNE9FS0xaM011TzdBU0xWQVR4cWZLdkV5OW9oZDdhU2NJVW5YYUNGM00',
+  vapidPublicKey: 'BDR1e93Tii3hjhtevwTbpXJjZf5nFfQOPT_-6vJ_kcFJgW9B7qoIZ465BG0Dg5EhcVSQ1HfBu0cne-nTd-pvDLg='
+})
